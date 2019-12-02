36ac91db2c5bacccdf5c8d72c9d54d55852147f4
caiorosseto <55113813+caiorosseto@users.noreply.github.com>
2019-12-02 12:08:24 -0300

    02-12-19

--- site/autores/caio/index.php
+++ site/autores/caio/index.php
@@ -2,137 +2,80 @@
 include('../../templates/header.php');
 
 ?>
-
-<main>
-
-    <div class="container">
-        <div class="row">
-
-
-            <div class="col-sm">
-                <div class="zoom">
-                    <img class="img-responsive" src="images/capa_new.jpg" alt="capa divida entre três hqs de ficção cientifica" />
-                    <h2 class="text">CAPA</h2>
-
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <div class="zoom"> <img class="img-responsive" src="images/capa_new.jpg" alt="capa divida entre três hqs de ficção cientifica" />
+                        <h2 class="text">CAPA</h2> </div>
+                </div>
+                <div class="col-sm">
+                    <div class="zoom"> <img class="img-responsive" src="images/matrix_300x150.jpg" alt="Capa da HQ Matix" /> </div>
+                    <h2>Matrix</h2>
+                    <br>
+                    <p>Descrição do quadrinho Matrix</p>
+                </div>
+                <div class="col-sm">
+                    <div class="zoom"> <img class="img-responsive" src="images/Y_O_Ultimo_Homem_300x200.jpg" alt="capa do hq Y o último homem" />
+                        <h2 class="text">CAPA</h2> </div>
                 </div>
             </div>
-            <div class="col-sm">
-                <div class="zoom">
-                    <img class="img-responsive" src="images/matrix_300x150.jpg" alt="Capa da HQ Matix" />
+            <p> <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample5" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Decender  <img src="images/Descender80x80.jpg" /></a>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample6" aria-expanded="false" aria-controls="multiCollapseExample2">East of West <img src="images/East_of_West80x80.jpg" /></button>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample7" aria-expanded="false" aria-controls="multiCollapseExample2">Juiz Dredd <img src="images/Juiz_Dredd80x80.jpg" /></button>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample8" aria-expanded="false" aria-controls="multiCollapseExample2">O Incal <img src="images/O_Incal80x80.jpg" /></button>
+            </p>
+            <div class="row">
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample5">
+                        <div class="card card-body"> Descender, obra do aclamado Jeff Lemire, usa da ficção-científica como um conduíte para abordar aquela velha questão do que é ser humano, atingindo algumas das notas de obras como Blade Runner e Matrix. </div>
+                    </div>
                 </div>
-                <h2>Matrix</h2><br>
-                <p>Descrição do quadrinho Matrix</p>
-            </div>
-
-
-
-            <div class="col-sm">
-                <div class="zoom">
-                    <img class="img-responsive" src="images/Y_O_Ultimo_Homem_300x200.jpg" alt="capa do hq Y o último homem" />
-                    <h2 class="text">CAPA</h2>
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample6">
+                        <div class="card card-body"> East of West, é uma história de faroeste ambientada em uma versão distópica dos EUA, na qual a Guerra Civil nunca teve um fim definitivo, até que um cometa atinge o Kansas. East of West conta com uma alta carga de comentário sociopolítico em sua narrativa, trazendo teorias conspiratórias acerca do apocalipse e fazendo com que o destino da humanidade repouse nas mãos dos Quatro Cavaleiros do Apocalipse. </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-
-
-        <div class="row">
-            <div class="col">
-                <div class="btn-group">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Alguma ação</a>
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample7">
+                        <div class="card card-body"> Agora, para quem curte ação caótica, Juiz Dredd é a pedida certa. A ambientação é um futuro ultra violento pós-apocalíptico no qual o juiz é responsável pelos cargos de polícia, juiz, júri e executor, aliando-se a outros juízes para controlar a violência excessiva de Mega City One. </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample8">
+                        <div class="card card-body"> Escrita pelo célebre cineasta Alejandro Jodorowsky e ilustrada por Moebius, a saga retratada em O Incal é uma viagem estarrecedora aos sentidos do leitor. A trama segue o detetive John Difool, que acaba recebendo um poderosos cristal conhecido como Incal de um alienígena prestes a morrer. O problema é que o cristal é item de interesse para diversas facções intergalácticas, o que coloca Difool junto de seu pássaro de estimação, a gaivota de concreto, a procurarem aliados inusitados para decidirem o que fazer com o Incal. </div>
                     </div>
                 </div>
             </div>
-
-            <div class="col">
-                <div class="btn-group">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Alguma ação</a>
+            <p> <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Paper Girls  <img src="images/Paper_Girls80x80.jpg" /></a>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Projeto Manhattan <img src="images/Projeto_Manhattan80x80.jpg" /></button>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample2">Saga <img src="images/Saga80x80.jpg" /></button>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample4" aria-expanded="false" aria-controls="multiCollapseExample2">Transmetropolitan <img src="images/Transmetropolitan80x80.jpg" /></button>
+            </p>
+            <div class="row">
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample1">
+                        <div class="card card-body"> Mais uma de Brian K. Vaughan na lista, desta vez temos Paper Girls, concebida em parceria com Cliff Chiang. Aqui, temos uma história que remete a obras como Stranger Things, na qual garotas suburbanas acabam encontrando alienígenas no planeta Terra, bem no dia das Bruxas de 1988. </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="col">
-                <div class="btn-group">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Alguma ação</a>
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample2">
+                        <div class="card card-body"> Manhattan Projects é um prato cheio para quem curte histórias "e se". No cenário proposto pela trama, temos o infame Projeto Manhattan da Segunda Guerra Mundial indo além da criação da Bomba Atômica, dedicando-se a ainda mais experimentos. Os personagens que compõem a narrativa incluem figuras célebres do mundo da ciência, como Albert Einstein e Wernher Von Braun. </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="col">
-                <div class="btn-group">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Alguma ação</a>
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample3">
+                        <div class="card card-body"> Uma das Space Operas mais aclamadas dos quadrinhos contemporâneos, Saga traz um universo exuberante que abarca diversos personagens únicos e cativantes. A obra é escrita por Brian K. Vaughan, com belíssimas ilustrações de Fiona Staples. A trama desponta com os amantes Alana e Marko, que pertencem a dois planetas distintos que se encontram em guerra. </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample4">
+                        <div class="card card-body"> Agora, para os fãs de um bom cyberpunk, vamos à essa obra de Warren Ellis. Transmetropolitan conta como personsagem central o jornalista Spider Jerusalem, que tenta combater a corrupção de políticos que concorrem à presidência dos EUA. A narrativa aborda diversas temáticas, passando por nanorrobótica e liberdade de expressão até seres extraterrestres e criogenia. </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col">
-                <div class="btn-group">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Alguma ação</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="btn-group">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Alguma ação</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="btn-group">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Alguma ação</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="btn-group">
-                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Alguma ação</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-
-
-
-
-</main>
-<?php
+    </main>
+    <?php
 include('../../templates/footer.php');
 ?>
